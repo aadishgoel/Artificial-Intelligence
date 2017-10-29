54e53f75a63b0bde5eeff70acbd0a656a75013dc
Aadish Goel <aadishgoel2013@users.noreply.github.com>
2017-10-29 15:30:29 +0530

    Add files via upload

--- wordSenseDisambiguation/try.py
+++ wordSenseDisambiguation/try.py
@@ -1,17 +1,24 @@
 from nltk.tokenize import word_tokenize
 from nltk.wsd import lesk
 from nltk.corpus import wordnet as wn
+import pywsd.lesk as wsd
 
+#s = 'I went to the bank to deposit money.', 'I was walking around a river bank while looking at fishes in the water.'
+#s = [ 'willows lined the bank of the stream' ]
+#s= [ 'a bank account has 30 thousand rupees' ]
 
-#sentences = 'I went to the bank to deposit money.', 'I was walking around a river bank while looking at fishes in the water.'
-sentences = 'I park my car in the parking area of an appartment.', 'We go to park daily for jogging.'
+#s = ['I take my car in a parking lot and park it there.']
+#s = ['Park is filled with trees and childrens play cricket in it']
 
+ambiguous = 'bank'
+#ambiguous = 'park'
 
-for sent in sentences:
-    words = word_tokenize(sent)
-    ans = lesk(words,'park')
-    print(ans,ans.definition())
-
+for fn in [lesk, wsd.original_lesk, wsd.simple_lesk, wsd.adapted_lesk, wsd.cosine_lesk]:
+    for sent in s:
+        #words = word_tokenize(sent)
+        ans = fn(sent,ambiguous)
+        print(ans,ans.definition() if ans else 'Not Found')
+    print()
 '''
 for i in wn.synsets('bank'):
     print(i)
