6d123e54436601dafcfdff59cb1428683f4a67b0
Aadish Goel <aadishgoel2013@users.noreply.github.com>
2017-10-29 19:20:05 +0530

    Add files via upload

--- wordSenseDisambiguation/wsd .py
+++ wordSenseDisambiguation/wsd .py
@@ -3,10 +3,10 @@ from nltk.corpus import stopwords
 import string
 import pywsd
 
-#p = input() 
-p = 'I was walking around a river bank while looking at fishes in the water.'
+p = input() 
+#p = 'I was walking around a river bank while looking at fishes in the water.'
 
-new= [ word for word in word_tokenize(p) if word not in stopwords.words('English') and word not in string.punctuation]
+new= [ word for word in word_tokenize(p) if word.lower() not in stopwords.words('English') and word not in string.punctuation]
 new = ' '.join(new)
 for word, context in pywsd.disambiguate(new):
     print(word.ljust(15,' '),':  ' ,context.definition() if context else 'Not Found' )
